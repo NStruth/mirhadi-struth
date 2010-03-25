c57e24757ead9207002844af868786e9e3ae22d3
Audiofreq <Audiofreq@255192e3-b35b-4917-3d57-a32ca832759d>
2010-03-25 20:37:16 +0000

    Threaded the teller Changed gui - empty

---
+++ trunk/BankSimPart2/src/com/uni/gui/GuiMain.java
@@ -0,0 +1,12 @@
+package com.uni.gui;
+
+import javax.swing.JFrame;
+
+public class GuiMain extends JFrame{
+
+	public GuiMain(){
+		super();
+		this.setBounds(50, 50, 800, 600);
+		this.setVisible(true);
+	}
+}
