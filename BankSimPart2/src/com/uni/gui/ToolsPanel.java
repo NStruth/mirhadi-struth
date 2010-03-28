ee205c30b0619db45b497aab6e15be20deb5903a
neildstruth <neildstruth@255192e3-b35b-4917-3d57-a32ca832759d>
2010-03-28 12:26:06 +0000

    Added sliders for teller queue and clock... Speeds may need tweeked

--- BankSimPart2/src/com/uni/gui/ToolsPanel.java
+++ BankSimPart2/src/com/uni/gui/ToolsPanel.java
@@ -9,8 +9,11 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import com.uni.main.BankSimulator;
+import com.uni.main.Statistics;
 
 public class ToolsPanel extends JPanel implements ActionListener{
 
@@ -32,13 +35,32 @@ public class ToolsPanel extends JPanel implements ActionListener{
 		
 		p.add(new JLabel("Teller"));
 		JSlider tellerSlider = new JSlider(10, 5000);
+		tellerSlider.setMajorTickSpacing(1000);
+		tellerSlider.setMinorTickSpacing(100);
 		tellerSlider.setPaintTicks(true);
+		tellerSlider.setSnapToTicks(true);
+		tellerSlider.addChangeListener(new TellerListener());
 		p.add(tellerSlider);
 		
 		p.add(new JLabel("Queue"));
 		JSlider queueSlider = new JSlider(10, 5000);
+		queueSlider.setMajorTickSpacing(1000);
+		queueSlider.setMinorTickSpacing(100);
 		queueSlider.setPaintTicks(true);
+		queueSlider.setSnapToTicks(true);
+		queueSlider.addChangeListener(new QueueListener());
 		p.add(queueSlider);
+		
+		p.add(new JLabel("Clock"));
+		JSlider clockSlider = new JSlider(200, 2000);
+		clockSlider.setMajorTickSpacing(100);
+		clockSlider.setMinorTickSpacing(100);
+		clockSlider.setPaintTicks(true);
+		clockSlider.setSnapToTicks(true);
+		clockSlider.addChangeListener(new ClockListener());
+		p.add(clockSlider);
+		
+
 		
 		JButton startButton = new JButton("Start Simulation");
 		JButton stopButton = new JButton("Stop Simulation");
@@ -54,6 +76,7 @@ public class ToolsPanel extends JPanel implements ActionListener{
 		
 	}
 
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		if(arg0.getActionCommand().equals("Start Simulation")){
@@ -65,5 +88,30 @@ public class ToolsPanel extends JPanel implements ActionListener{
 			bs.stopSimulation();
 		}
 
+	}
+	class TellerListener implements ChangeListener {
+	    public void stateChanged(ChangeEvent e) {
+	        JSlider source = (JSlider)e.getSource();
+	        if (!source.getValueIsAdjusting()) {
+	            Statistics.TELLER_SPEED = (int)source.getValue();
+	        }    
+	    }
+	}
+	    
+	class QueueListener implements ChangeListener {
+	    public void stateChanged(ChangeEvent e) {
+		JSlider source = (JSlider)e.getSource();
+		if (!source.getValueIsAdjusting()) {
+			Statistics.GENERATOR_SPEED = (int)source.getValue();
+			}    
+	    }
+	}
+	class ClockListener implements ChangeListener {
+	    public void stateChanged(ChangeEvent e) {
+		JSlider source = (JSlider)e.getSource();
+		if (!source.getValueIsAdjusting()) {
+			Statistics.CLOCK_SPEED = (int)source.getValue();
+			}    
+	    }
 	}
 }
