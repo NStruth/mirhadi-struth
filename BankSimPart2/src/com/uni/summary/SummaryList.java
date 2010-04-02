f41183faf85c3e79bf791747a0969200050b296d
Audiofreq <Audiofreq@255192e3-b35b-4917-3d57-a32ca832759d>
2010-04-02 12:57:36 +0000

    SummaryList

--- BankSimPart2/src/com/uni/summary/SummaryList.java
+++ BankSimPart2/src/com/uni/summary/SummaryList.java
@@ -2,6 +2,7 @@ package com.uni.summary;
 
 import java.util.ArrayList;
 
+import com.uni.Teller.Teller;
 import com.uni.account.Transaction.Choices;
 import com.uni.customer.Customer;
 import com.uni.main.Statistics;
@@ -30,53 +31,84 @@ public class SummaryList extends ArrayList<Summary> {
 		return instance;
 	}
 	
-	public String getTotalWithdrawn(Customer c)
-	{
-		int withdraw = 0;
-		for(Summary s: this)
-			if(s.getC() == c && s.getType() == Choices.WITHDRAW)
-			{
-				withdraw += s.getAmount();
-			}
-			else
-				if(s.getC() == c && s.getType() == Choices.CLOSE)
-				{
-					withdraw += s.getAmount();
+	
+	
+	public String getCustomerStats(Customer c){
+		String retV = "";
+		int transactions = 0;
+		int totalWithdrawn = 0, withdraws = 0;
+		int totalDeposited = 0, deposits = 0;
+		int opened = 0, closed = 0;
+		
+		for(Summary s: this){
+			if(s.getC() == c){
+				transactions++;
+				if(s.getType() == Choices.DEPOSIT){
+					totalDeposited += s.getAmount();
+					deposits++;
 				}
-		return Statistics.toPoundsAndPence(withdraw);
+				if(s.getType() == Choices.WITHDRAW){
+					withdraws++;
+					totalWithdrawn += s.getAmount();
+				}
+				if(s.getType() == Choices.OPEN)
+					opened++;
+				if(s.getType() == Choices.CLOSE)
+					closed++;
+			}
+		}
+		
+		retV += "\n*******************\n";
+		retV+= "Customer Name:" + c.getFullName();
+		retV+="Total transactions:" +transactions + "\n";
+		retV+="\nTotal deposits:" + deposits + "\n";
+		retV+="Deposit amount:" + totalDeposited + "\n";
+		retV+="\nTotal withdraws:" + withdraws + "\n";
+		retV+="Withdraw amount:" + totalWithdrawn + "\n";
+		retV+="\nAccounts Opened:" + opened + "\n";
+		retV+="\nAccounts Closed:" + closed + "\n";
+		retV+="\n*******************\n";
+		
+		return "";
 	}
 	
-	public String getTotalDeposited(Customer c)
-	{
-		int deposited = 0;
-		for(Summary s: this)
-			if(s.getC() == c && s.getType() == Choices.DEPOSIT)
-			{
-				deposited += s.getAmount();
+	public String getTellerStats(Teller t){
+		String retV = "";
+		
+		int transactions = 0;
+		int totalWithdrawn = 0, withdraws = 0;
+		int totalDeposited = 0, deposits = 0;
+		int opened = 0, closed = 0;
+		
+		for(Summary s: this){
+			if(s.getT() == t){
+				transactions++;
+				if(s.getType() == Choices.DEPOSIT){
+					totalDeposited += s.getAmount();
+					deposits++;
+				}
+				if(s.getType() == Choices.WITHDRAW){
+					withdraws++;
+					totalWithdrawn += s.getAmount();
+				}
+				if(s.getType() == Choices.OPEN)
+					opened++;
+				if(s.getType() == Choices.CLOSE)
+					closed++;
 			}
-		return Statistics.toPoundsAndPence(deposited);
-	}
-	
-	public String getTotalOpen(Customer c)
-	{
-		int open = 0;
-		for(Summary s: this)
-			if(s.getC() == c && s.getType() == Choices.OPEN)
-			{
-				open++;
-			}
-		return "Total Opened = "+open;
-	}
-	
-	public String getTotalClosed(Customer c)
-	{
-		int closed = 0;
-		for(Summary s: this)
-			if(s.getC() == c && s.getType() == Choices.CLOSE)
-			{
-				closed++;
-			}
-		return "Total Closed = "+closed;
-	}
+		}
+		retV += "\n*******************\n";
+		retV+= "Teller id:" + t.getIdent();
+		retV+="Total transactions:" +transactions + "\n";
+		retV+="\nTotal deposits:" + deposits + "\n";
+		retV+="Deposit amount:" + totalDeposited + "\n";
+		retV+="\nTotal withdraws:" + withdraws + "\n";
+		retV+="Withdraw amount:" + totalWithdrawn + "\n";
+		retV+="\nAccounts Opened:" + opened + "\n";
+		retV+="\nAccounts Closed:" + closed + "\n";
+		retV+="\n*******************\n";
+		
+		return retV;
 
+	}
 }
