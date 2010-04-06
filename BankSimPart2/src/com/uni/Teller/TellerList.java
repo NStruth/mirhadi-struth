7274101caaa38d8431606a5af04bda1f0af264c3
Audiofreq <Audiofreq@255192e3-b35b-4917-3d57-a32ca832759d>
2010-04-06 19:53:54 +0000

    comments

--- BankSimPart2/src/com/uni/Teller/TellerList.java
+++ BankSimPart2/src/com/uni/Teller/TellerList.java
@@ -1,25 +1,32 @@
+/**
+ * @author Jon Mirhadi
+ * @author Neil Struth
+ * 
+ * @version 2.0
+ * 
+ * Stores a list of tellers
+ * 
+ */
 package com.uni.Teller;
 
 import java.util.ArrayList;
 
 public class TellerList extends ArrayList<Teller>{
 	
+	/**
+	 * Constructor for this teller list
+	 */
 	public TellerList(){
 		super();
 	}
 	
+	/**
+	 * 
+	 * Add a teller to the list
+	 * @param t the teller to be added
+	 */
 	public void addTeller(Teller t){
 		this.add(t);
-	}
-	
-	public void openTeller(int index){
-		//TODO
-		//this.get(index).open();
-	}
-	
-	public void closeTeller(int index){
-		//TODO
-		//this.get(index).close();
 	}
 
 }
